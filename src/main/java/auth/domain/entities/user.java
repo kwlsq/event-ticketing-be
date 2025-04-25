8a703c3afc34fa56385fe74e894e2c47fafffc82
Vincentius <vincentiussss@gmail.com>
2025-04-25 13:28:22 +0700

    feat: user entity

---
+++ src/main/java/auth/domain/entities/user.java
@@ -0,0 +1,76 @@
+package auth.domain.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Filter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+@Filter(name = "deletedAtFilter", condition = "deleted_at is null")
+public class user {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_id_gen")
+    @SequenceGenerator(name="users_id_gen",sequenceName = "users_id_seq",allocationSize = 1)
+    @Column(name="id",nullable = false)
+    private Integer id;
+
+    @Column(name="name",nullable = false,length = 255)
+    private String name;
+
+    @Column(name="email",nullable = false,length = 255)
+    private String email;
+
+    @Column(name="password",nullable = false,length = 255)
+    private String password;
+
+    @Column(name="msisdn",nullable = true,length = 20)
+    private String msisdn;
+
+    @ColumnDefault("now()")
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @ColumnDefault("now()")
+    @Column(name = "modified_at")
+    private Instant modifiedAt;
+
+    @Column(name = "deleted_at")
+    private Instant deletedAt;
+
+//   one ticket one user
+
+//   one user many invoice
+
+//   one referrer one referral
+
+//   one referral many referee
+
+//   one user many point
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+        modifiedAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        modifiedAt = Instant.now();
+    }
+
+    @PreRemove
+    public void preRemove() {
+        deletedAt = Instant.now();
+    }
+}
