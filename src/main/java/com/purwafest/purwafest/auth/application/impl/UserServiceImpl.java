55ec4fc7c8250fad5be136970fa451b75464f86b
Vincentius <vincentiussss@gmail.com>
2025-04-28 16:20:38 +0700

    feat: login without auth

--- src/main/java/com/purwafest/purwafest/auth/application/impl/UserServiceImpl.java
+++ src/main/java/com/purwafest/purwafest/auth/application/impl/UserServiceImpl.java
@@ -4,11 +4,13 @@ import com.purwafest.purwafest.auth.application.UserService;
 import com.purwafest.purwafest.auth.domain.entities.User;
 import com.purwafest.purwafest.auth.domain.enums.UserType;
 import com.purwafest.purwafest.auth.domain.exceptions.DuplicateUserException;
+import com.purwafest.purwafest.auth.domain.exceptions.LoginFailedException;
 import com.purwafest.purwafest.auth.infrastructure.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -38,6 +40,21 @@ public class UserServiceImpl implements UserService {
         //Set type base on registrationType, save to DB, then return the result of newly registered user
         request.setUserType(registrationType.equalsIgnoreCase("user")? UserType.USER:UserType.ORGANIZER);
         return userRepository.save(request);
+    }
+
+    @Override
+    public User login(User request, String loginType){
+        // Convert registrationType to uppercase and map it to the UserType enum
+        UserType userType = UserType.valueOf(loginType.toUpperCase());
+
+        Optional<User> foundUser = userRepository.findUserByEmailAndUserType(request.getEmail(),userType);
+//        authencticate
+
+        if(request.getPassword().equals(foundUser.get().getPassword())){
+            return foundUser.get();
+        } else {
+            throw new LoginFailedException("Invalid email or password");
+        }
     }
 
 }
