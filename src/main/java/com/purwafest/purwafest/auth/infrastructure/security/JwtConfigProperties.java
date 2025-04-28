0eb1b114a4ff16415ab6b11285c579402f781012
Vincentius <vincentiussss@gmail.com>
2025-04-28 22:37:20 +0700

    feat: security config

---
+++ src/main/java/com/purwafest/purwafest/auth/infrastructure/security/JwtConfigProperties.java
@@ -0,0 +1,11 @@
+package com.purwafest.purwafest.auth.infrastructure.security;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "jwt")
+@Data
+public class JwtConfigProperties {
+  private String secret;
+  private String refreshSecret;
+}
--- src/main/java/com/purwafest/purwafest/auth/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/purwafest/purwafest/auth/infrastructure/security/SecurityConfig.java
@@ -1,23 +1,97 @@
 package com.purwafest.purwafest.auth.infrastructure.security;
 
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import com.purwafest.purwafest.auth.application.UserService;
 import jakarta.servlet.http.Cookie;
+import lombok.extern.java.Log;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Security;
+
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@Log
 public class SecurityConfig {
+    private final PasswordEncoder passwordEncoder;
+    private final JwtConfigProperties jwtConfigProperties;
+    private final UserService userService;
+
+    public SecurityConfig(PasswordEncoder passwordEncoder, JwtConfigProperties jwtConfigProperties, UserService userService){
+        this.passwordEncoder = passwordEncoder;
+        this.jwtConfigProperties = jwtConfigProperties;
+        this.userService = userService;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(authenticationProvider);
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+//                        .anyRequest().permitAll()
+                          .requestMatchers("/api/v1/event").authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2->{
+                    oauth2.jwt(jwt->jwt.decoder(jwtDecoder()));
+                    oauth2.bearerTokenResolver(request->{
+                        Cookie[] cookies = request.getCookies();
+                        if(cookies != null){
+                            for(Cookie cookie : cookies){
+                                if(cookie.getName().equals("SID")){
+                                    return cookie.getValue();
+                                }
+                            }
+                        }
+                        String token = request.getHeader("Authorization");
+                        if(token != null && token.startsWith("Bearer ")){
+                            return token.substring(7);
+                        }
+                        return null;
+                    });
+                })
+                .userDetailsService(userService)
                 .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        SecretKey secretKey = new SecretKeySpec(jwtConfigProperties.getSecret().getBytes(),"HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder(){
+        SecretKey secretKey = new SecretKeySpec(jwtConfigProperties.getSecret().getBytes(),"HmacSHA256");
+        JWKSource<SecurityContext> immutableSecret = new ImmutableSecret<SecurityContext>(secretKey);
+        return new NimbusJwtEncoder(immutableSecret);
     }
 }
