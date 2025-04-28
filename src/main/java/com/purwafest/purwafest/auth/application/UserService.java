bcfc97312ffb61173f2904b6073145a8b5e07992
Vincentius <vincentiussss@gmail.com>
2025-04-28 22:37:20 +0700

    feat: security config

--- src/main/java/com/purwafest/purwafest/auth/application/UserService.java
+++ src/main/java/com/purwafest/purwafest/auth/application/UserService.java
@@ -1,10 +1,11 @@
 package com.purwafest.purwafest.auth.application;
 
 import com.purwafest.purwafest.auth.domain.entities.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     User register(User request, String registrationType);
     User login(User request, String registrationType);
     List<User> getAll();
