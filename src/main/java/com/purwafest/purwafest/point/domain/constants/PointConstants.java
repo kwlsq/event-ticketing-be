20400d49d4e388479fa5b90f6200c92970259828
Vincentius <vincentiussss@gmail.com>
2025-05-13 14:33:29 +0700

    feat:add point to referrer  after referral code is used

--- src/main/java/com/purwafest/purwafest/point/domain/constants/PointConstants.java
+++ src/main/java/com/purwafest/purwafest/point/domain/constants/PointConstants.java
@@ -1,4 +1,6 @@
 package com.purwafest.purwafest.point.domain.constants;
 
 public class PointConstants {
+    public static final long REFERRAL_POINT_VALUE = 10000L;
+    public static final String POINT_SOURCE =  "referral";
 }
